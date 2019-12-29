f0f6b429c4d48194183fc931e497f73381eee0de
ThomasBarnekow <thomas@barnekow.info>
2019-12-29 13:02:48 +0100

    Add further tests to XmlTransformationTests

--- CodeSnippets.Tests/OpenXml/Wordprocessing/XmlTransformationTests.cs
+++ CodeSnippets.Tests/OpenXml/Wordprocessing/XmlTransformationTests.cs
@@ -7,9 +7,12 @@
 // Email: thomas<at/>barnekow<dot/>info
 
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using Xunit;
 
@@ -34,19 +37,6 @@ namespace CodeSnippets.Tests.OpenXml.Wordprocessing
             @"<w:r><w:rPr><w:i /></w:rPr><w:t>Conversion of written languages</w:t></w:r>" +
             @"<w:r><w:t>.</w:t></w:r>" +
             @"</w:p>";
-
-        [Fact]
-        public void CanTransformXmlToOpenXml()
-        {
-            // Arrange, creating an XElement based on the given XML.
-            var xmlParagraph = XElement.Parse(Xml);
-
-            // Act, transforming the XML into Open XML.
-            var paragraph = (Paragraph) TransformElementToOpenXml(xmlParagraph);
-
-            // Assert, demonstrating that we have indeed created an Open XML Paragraph instance.
-            Assert.Equal(OuterXml, paragraph.OuterXml);
-        }
 
         private static OpenXmlElement TransformElementToOpenXml(XElement element)
         {
@@ -77,6 +67,139 @@ namespace CodeSnippets.Tests.OpenXml.Wordprocessing
                     ? new EnumValue<SpaceProcessingModeValues>(SpaceProcessingModeValues.Preserve)
                     : null
             };
+        }
+
+        private const string MoviesXml =
+            @"<?xml version=""1.0"" encoding=""UTF-8""?>
+<Movies>
+  <Movie>
+    <Name>Crash</Name>
+    <Released>2005</Released>
+  </Movie>
+  <Movie>
+    <Name>The Departed</Name>
+    <Released>2006</Released>
+  </Movie>
+  <Movie>
+    <Name>The Pursuit of Happiness</Name>
+    <Released>2006</Released>
+  </Movie>
+  <Movie>
+    <Name>The Bucket List</Name>
+    <Released>2007</Released>
+  </Movie>
+</Movies>";
+
+        private static Table TransformMovies(XElement movies)
+        {
+            var headerRow = new[]
+            {
+                new TableRow(movies
+                    .Elements()
+                    .First()
+                    .Elements()
+                    .Select(e => new TableCell(new Paragraph(new Run(new Text(e.Name.LocalName))))))
+            };
+            IEnumerable<OpenXmlElement> movieRows = movies.Elements().Select(TransformMovie);
+
+            return new Table(headerRow.Concat(movieRows));
+        }
+
+        private static OpenXmlElement TransformMovie(XElement element)
+        {
+            return element.Name.LocalName switch
+            {
+                "Movie" => new TableRow(element.Elements().Select(TransformMovie)),
+                _ => new TableCell(new Paragraph(new Run(new Text(element.Value))))
+            };
+        }
+
+        private static void InsertTable(WordprocessingDocument wordprocessingDocument)
+        {
+            const string xmlFile = @"Resources\Movies.xml";
+            MainDocumentPart mainPart = wordprocessingDocument.MainDocumentPart;
+
+            // How to read the XML and create a table in word document filled with data from XML
+
+            // First, read the Movies XML string from your XML file.
+            string moviesXml = File.ReadAllText(xmlFile);
+
+            // Second, create the table as previously shown in the unit test method.
+            XElement movies = XElement.Parse(moviesXml);
+            Table table = TransformMovies(movies);
+
+            // Third, append the Table to your empty Body.
+            mainPart.Document.Body.AppendChild(table);
+        }
+
+        private static void AssertTableIsCorrect(XElement movies, Table table)
+        {
+            static (string Name, string Released) GetRowTexts(XElement movie)
+            {
+                string name = movie.Element("Name")?.Value ?? throw new ArgumentException();
+                string released = movie.Element("Released")?.Value ?? throw new ArgumentException();
+                return (name, released);
+            }
+
+            static bool HasRowTexts(TableRow tr, (string Name, string Released) rowTexts)
+            {
+                (string name, string released) = rowTexts;
+
+                return tr.Elements<TableCell>().ElementAt(0).Descendants<Text>().Any(t => t.Text == name) &&
+                       tr.Elements<TableCell>().ElementAt(1).Descendants<Text>().Any(t => t.Text == released);
+            }
+
+            Assert.All(
+                movies.Elements().Select(GetRowTexts),
+                rowTexts => Assert.Contains(table.Elements<TableRow>(), tr => HasRowTexts(tr, rowTexts)));
+        }
+
+        private static XElement GetMovies()
+        {
+            const string path = @"Resources\Movies.xml";
+            string moviesXml = File.ReadAllText(path);
+            return XElement.Parse(moviesXml);
+        }
+
+        [Fact]
+        public void CanCreateTableFromXml()
+        {
+            XElement movies = XElement.Parse(MoviesXml);
+            Table table = TransformMovies(movies);
+
+            Assert.NotNull(table);
+        }
+
+        [Fact]
+        public void CanInsertTable()
+        {
+            // Arrange, creating a new WordprocessingDocument.
+            using var stream = new MemoryStream();
+            using var wordDocument = WordprocessingDocument.Create(stream, WordprocessingDocumentType.Document);
+
+            MainDocumentPart mainPart = wordDocument.AddMainDocumentPart();
+            mainPart.Document = new Document(new Body());
+
+            // Act, inserting the table.
+            InsertTable(wordDocument);
+
+            // Assert, demonstrating the table was added.
+            XElement movies = GetMovies();
+            Table table = wordDocument.MainDocumentPart.Document.Body.Elements<Table>().Single();
+            AssertTableIsCorrect(movies, table);
+        }
+
+        [Fact]
+        public void CanTransformXmlToOpenXml()
+        {
+            // Arrange, creating an XElement based on the given XML.
+            XElement xmlParagraph = XElement.Parse(Xml);
+
+            // Act, transforming the XML into Open XML.
+            var paragraph = (Paragraph) TransformElementToOpenXml(xmlParagraph);
+
+            // Assert, demonstrating that we have indeed created an Open XML Paragraph instance.
+            Assert.Equal(OuterXml, paragraph.OuterXml);
         }
     }
 }
